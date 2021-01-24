26f1a243c8b890d990ba9d09d94d8bc38fba2b15
Marcos <marcos@nuvemshop.com.br>
2021-01-23 22:50:14 -0300

    Update [user].js

--- pages/api/user/[user].js
+++ pages/api/user/[user].js
@@ -2,22 +2,20 @@ const { LinkedInProfileScraper } = require('linkedin-profile-scraper')
 
 export default function handler(req, res) {
     const {query: { user }} = req
-    // const endpoint = `https://www.linkedin.com/in/${user}`;
-    // // Plain Javascript
-    // (async() => {
-    // const scraper = new LinkedInProfileScraper({
-    //     sessionCookieValue: 'AQEDAQNUJiMAUTz4AAABdy-VECwAAAF3U6GULE4AYkvABa-dUWhEp89fhW8GVb8GDpICEPbgz_XuS8PWlzhR_5GWuRULKqJW-zlYMlGVvehODd9DKZAHq-gv9s1eTKxPO_daiks_20PO232gXYrkY_j5',
-    //     keepAlive: false
-    // });
+    const endpoint = `https://www.linkedin.com/in/${user}`;
+    // Plain Javascript
+    (async() => {
+    const scraper = new LinkedInProfileScraper({
+        sessionCookieValue: 'AQEDAQNUJiMAUTz4AAABdy-VECwAAAF3U6GULE4AYkvABa-dUWhEp89fhW8GVb8GDpICEPbgz_XuS8PWlzhR_5GWuRULKqJW-zlYMlGVvehODd9DKZAHq-gv9s1eTKxPO_daiks_20PO232gXYrkY_j5',
+        keepAlive: false
+    });
 
-    // // Prepare the scraper
-    // // Loading it in memory
-    // await scraper.setup()
+    // Prepare the scraper
+    // Loading it in memory
+    await scraper.setup()
 
-    // const result = await scraper.run(endpoint)
+    const result = await scraper.run(endpoint)
 
-    // res.status(200).json(result)
-    // })()
-
-    res.status(200).json(user)
+    res.status(200).json(result)
+    })()
 }
