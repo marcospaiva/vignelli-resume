8fc4a59e69f5a241ff266edaffe323ead4f07c62
Marcos <marcos@nuvemshop.com.br>
2021-01-23 22:56:04 -0300

    Update [user].js

--- pages/api/user/[user].js
+++ pages/api/user/[user].js
@@ -7,7 +7,7 @@ export default function handler(req, res) {
     (async() => {
     const scraper = new LinkedInProfileScraper({
         sessionCookieValue: 'AQEDAQNUJiMAUTz4AAABdy-VECwAAAF3U6GULE4AYkvABa-dUWhEp89fhW8GVb8GDpICEPbgz_XuS8PWlzhR_5GWuRULKqJW-zlYMlGVvehODd9DKZAHq-gv9s1eTKxPO_daiks_20PO232gXYrkY_j5',
-        keepAlive: false
+        keepAlive: true
     });
 
     // Prepare the scraper
