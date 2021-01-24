b2f3d6ad375f48c37114f93f20258de86457bcb4
Marcos <marcos@nuvemshop.com.br>
2021-01-23 21:23:15 -0300

    add server

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { LinkedInProfileScraper } = require('linkedin-profile-scraper');
+const getUser = require('./routes')
+
+app.use(cors());
+
+app.get('/user/:userId', function (req, res) {
+
+    const endpoint = `https://www.linkedin.com/in/${req.params['userId']}`;
+
+    // Plain Javascript
+    (async() => {
+    const scraper = new LinkedInProfileScraper({
+        sessionCookieValue: 'AQEDAQNUJiMAUTz4AAABdy-VECwAAAF3U6GULE4AYkvABa-dUWhEp89fhW8GVb8GDpICEPbgz_XuS8PWlzhR_5GWuRULKqJW-zlYMlGVvehODd9DKZAHq-gv9s1eTKxPO_daiks_20PO232gXYrkY_j5',
+        keepAlive: false
+    });
+
+    // Prepare the scraper
+    // Loading it in memory
+    await scraper.setup()
+
+    const result = await scraper.run(endpoint)
+
+    res.json(result)
+    })()
+
+
+})
+
+
+
+app.listen(process.env.PORT || 3001)
