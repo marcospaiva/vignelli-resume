8f1cfb010fb8815eb0694b905e14ed0c1b048b39
Marcos <marcos@nuvemshop.com.br>
2021-01-24 11:59:12 -0300

    Add layout

---
+++ pages/components/layout.js
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Header from './Header'
+
+
+export default function Layout({
+    children,
+    title = 'Default title',
+}) {
+    return (
+        <div>
+            <Head>
+                <title>{title}</title>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+            <main className="grid-container">
+            <Header></Header>
+
+            {children}
+            </main>
+        </div>
+    )
+}
