4a882299f26fd7f53c3304a29498a507c2ab5721
Marcos <marcos@nuvemshop.com.br>
2021-01-24 23:07:57 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import Layout from './components/layout'
+import Header from './components/Header'
+import Showcase from './components/Showcase'
+import CTA from './components/CTA'
 
 export default function HomePage() {
     return (
         <Layout title="Welcome to vignelli resume">
+            <Header></Header>
+            <Showcase></Showcase>
+            <CTA></CTA>
         </Layout>
     )
 }
