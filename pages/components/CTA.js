c169cbe357d4ca119a37c82ab63f3c7c53c80500
Marcos Paiva <marcos@nuvemshop.com.br>
2021-01-26 07:30:14 -0300

    Getting data from API and passing to the page (#6)

--- pages/components/CTA.js
+++ pages/components/CTA.js
@@ -1,15 +1,26 @@
 import style from '../styles/CTA.module.css'
 import f from '../styles/Form.module.css'
 
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 export default function CTA() {
+    const router = useRouter();
+    const [query, setQuery] = useState([]);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        router.push(`/resume/${query}`)
+    }
+    const handleParam = setValue => e => setValue(e.target.value)
+
     return (
         <section id="cta" className={style.Section}>
             <h2>Get started</h2>
-            <form action="/" className={f.form}>
+            <form className={f.form} onSubmit={handleSubmit}>
                 <div className={f.inputGroup}>
-                    <label for="linkedin_name" className={f.inputPrefix}>https://linkedin.com/in/</label>
-                    <input type="text" id="linkedin_name" name="linkedin_name" className={f.input}/>
+                    <label htmlFor="linkedin_name" className={f.inputPrefix}>https://linkedin.com/in/</label>
+                    <input type="text" id="linkedin_name" value={query} onChange={handleParam(setQuery)} name="linkedin_name" className={f.input}/>
                 </div>
             </form>
         </section>
